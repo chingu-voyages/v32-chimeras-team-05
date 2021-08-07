7ad072fab0e2c462a2a5ad801347651dc04fc1f6
Edwin Agbenyega <eddybanks03@gmail.com>
2021-08-06 23:35:02 -0700

    Add initial landing page layout

--- nudev-frontend/src/App.js
+++ nudev-frontend/src/App.js
@@ -1,4 +1,3 @@
-import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "./components/layout/Navigation";
 import About from "./pages/About";
@@ -6,20 +5,20 @@ import Landing from "./pages/Landing";
 import Resources from "./pages/Resources";
 import Team from "./pages/Team";
 import AuthPage from "./pages/AuthPage";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <Container>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/resources" component={Resources} />
-          <Route exact path="/team" component={Team} />
-          <Route exact path="/login" component={AuthPage} />
-        </Switch>
-      </Container>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/resources" component={Resources} />
+        <Route exact path="/team" component={Team} />
+        <Route exact path="/login" component={AuthPage} />
+      </Switch>
+      <Footer />
     </Router>
   );
 }
